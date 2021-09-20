b73bf08beddb6fa6f83514609e9693b6c680ee05
Michael Bamford <michael@michaelbamford.com>
2021-09-20 12:28:50 +1000

    ommit
    Add Recipe show view with attrs table

--- views/recipes/show.rb
+++ views/recipes/show.rb
@@ -1,7 +1,21 @@
+require 'tty-table'
+
 module Views
     module Recipes
         def self.show(recipe)
-            puts recipe.inspect
+            # What happens if recipe.nil? Need a guard?
+
+            headers = %w[Atrribute Value]
+            attrs = attribute_rows_for recipe
+            table = TTY::Table.new headers, attrs
+            puts table.render :ascii
+        end
+
+        def self.attribute_rows_for(recipe)
+            rows = []
+            rows << ['Id', recipe.id]
+            rows << ['Name', recipe.name]
+            rows << ['Difficulty', recipe.difficulty]
         end
     end
 end
